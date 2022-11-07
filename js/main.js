81992dea13017f24985a5d5c7caa733397c77a1a
TomasBelfonte <tomas.belfonte@gmail.com>
2022-11-07 19:14:30 +0100

    spesa

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const listaSpesa = [ "pane", "pasta", "formaggio", "burro", "sugo", ];
+
+let i = 0;
+
+elementContainer = document.querySelector (".container");
+
+while ( i < listaSpesa.length ) {
+    
+    let listaEsaminata = listaSpesa[i];
+
+    i++;
+
+    console.log (listaEsaminata);
+
+    
+}
